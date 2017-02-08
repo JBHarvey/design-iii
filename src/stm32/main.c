b9403e00a47c4fc03bb46e0af5b0a235eb55261e
Martin31416 <Jesus_SoldierZ@hotmail.com>
2017-02-07 23:21:24 -0500

    ajouts fichiers pour compter rising edge external interrupt

---
+++ src/stm32/main.c
@@ -0,0 +1,120 @@
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  Ac6
+ * @version V1.0
+ * @date    01-December-2013
+ * @brief   Default main function.
+ ******************************************************************************
+ */
+
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
+#include "stm32f4xx_exti.h"
+#include "stm32f4xx_syscfg.h"
+#include "misc.h"
+
+volatile uint8_t numberOfFrontEdges1 = 0;
+
+/* Configure pins to be interrupts */
+void Configure_PD0(void) {
+	/* Set variables used */
+	GPIO_InitTypeDef GPIO_InitStruct;
+	EXTI_InitTypeDef EXTI_InitStruct;
+	NVIC_InitTypeDef NVIC_InitStruct;
+
+	/* Enable clock for GPIOD */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	/* Enable clock for SYSCFG */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+	/* Set pin as input */
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+	/* Tell system that you will use PD0 for EXTI_Line0 */
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOD, EXTI_PinSource0);
+
+	/* PD0 is connected to EXTI_Line0 */
+	EXTI_InitStruct.EXTI_Line = EXTI_Line0;
+	/* Enable interrupt */
+	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+	/* Interrupt mode */
+	EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+	/* Triggers on rising and falling edge */
+	EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+	/* Add to EXTI */
+	EXTI_Init(&EXTI_InitStruct);
+
+	/* Add IRQ vector to NVIC */
+	/* PD0 is connected to EXTI_Line0, which has EXTI0_IRQn vector */
+	NVIC_InitStruct.NVIC_IRQChannel = EXTI0_IRQn;
+	/* Set priority */
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+	/* Set sub priority */
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+	/* Enable interrupt */
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	/* Add to NVIC */
+	NVIC_Init(&NVIC_InitStruct);
+}
+
+void intializeLeds() {
+	/*GPIO_InitTypeDef GPIO_InitStruct;
+	 GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	 GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
+	 GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+	 GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	 GPIO_InitStruct.GPIO_Pin = LED6;*/
+	STM_EVAL_LEDInit (LED6);
+}
+
+void systickInit(uint16_t frequency) {
+	RCC_ClocksTypeDef RCC_Clocks;
+	RCC_GetClocksFreq(&RCC_Clocks);
+	(void) SysTick_Config(RCC_Clocks.HCLK_Frequency / frequency);
+}
+
+int main() {
+	/* Initialize system */
+	SystemInit();
+	/* set internal interrupt*/
+	systickInit(1);
+
+	/* Configure PD0 as interrupt */
+	Configure_PD0();
+
+	/* Initialize LEDS */
+	intializeLeds();
+
+	while (1) {
+
+	}
+}
+
+extern void SysTick_Handler(void) {
+	int total = numberOfFrontEdges1;
+	numberOfFrontEdges1 = 0;
+}
+
+/* Set interrupt handlers */
+/* Handle PD0 interrupt */
+void EXTI0_IRQHandler(void) {
+	/* Make sure that interrupt flag is set */
+	if (EXTI_GetITStatus(EXTI_Line0) != RESET) {
+		/* Do your stuff when PD0 is changed */
+
+		/* Toggle RED led */
+		STM_EVAL_LEDToggle (LED6);
+		/* Check counter */
+		numberOfFrontEdges1++;
+
+		/* Clear interrupt flag */
+		EXTI_ClearITPendingBit (EXTI_Line0);
+	}
+}
+
