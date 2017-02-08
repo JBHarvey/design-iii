d1f8464ae111b9f640155b0cd6c75bdd611e5278
yan-m <yannik.marek.1@ulaval.ca>
2017-02-07 19:51:11 -0500

    Print percent tests succeeded.

--- test/execute_test.sh
+++ test/execute_test.sh
@@ -22,7 +22,13 @@ done
 
 echo suceeded: $suceeded_tests
 echo failed: $failed_tests
- 
+
+total_tests=$(($suceeded_tests + $failed_tests))
+percent_succeed=$(echo "scale=2; $suceeded_tests*100/$total_tests" | bc)
+
+echo Percent succeeded: $percent_succeed %
+echo
+
 if [ "$failed_tests" -eq "0" ]; then
    exit 0;
 fi
