445d3777368055e540e11d08a4c7552d19c64676
yan-m <yannik.marek.1@ulaval.ca>
2017-02-11 16:10:04 -0500

    Put criterion binary in repo.

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,3 +1,3 @@
- clang -Ofast  -o StateTest StateTest.c ../src/State.c -g -lopencv_core -lm -lopencv_imgproc -lopencv_highgui -fsanitize=address -fno-omit-frame-pointer -I ../src/ -I ../criterion/include/ ../criterion/lib/libcriterion.so
+ clang -Ofast  -o testsuite *.c ../src/*.c -g -lopencv_core -lm -lopencv_imgproc -lopencv_highgui -fsanitize=address -fno-omit-frame-pointer -I ../src/ -I ../criterion/include/ ../criterion/lib/libcriterion.so
  
-LD_LIBRARY_PATH=`pwd`/../criterion/lib/ ./StateTest
+LD_LIBRARY_PATH=`pwd`/../criterion/lib/ ./testsuite
