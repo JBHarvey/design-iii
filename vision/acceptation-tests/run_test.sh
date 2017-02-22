93c5e3c42fbfb68d2166818d069a957c271b7b6c
yan-m <yannik.marek.1@ulaval.ca>
2017-02-22 14:48:53 -0500

    Space out tolerances equally.

--- vision/acceptation-tests/run_test.sh
+++ vision/acceptation-tests/run_test.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 SUCCEEDED_IMAGES=()
 
+TOLERANCE=124.0 ./build.sh || exit 1
+
+echo execute with tolerance 1000/124 +-0.21cm
+. ./execute_test.sh
+
+
+TOLERANCE=111.0 ./build.sh || exit 1
+
+echo execute with tolerance 1000/111 +-0.27cm
+. ./execute_test.sh
+
 TOLERANCE=90.0 ./build.sh || exit 1
 
 echo execute with tolerance 1000/90 +-0.33cm
-. ./execute_test.sh
-
-TOLERANCE=83.0 ./build.sh || exit 1
-
-echo execute with tolerance 1000/83 +-0.36cm
 . ./execute_test.sh
 
 TOLERANCE=76.0 ./build.sh || exit 1
@@ -16,19 +22,9 @@ TOLERANCE=76.0 ./build.sh || exit 1
 echo execute with tolerance 1000/76 +-0.39cm
 . ./execute_test.sh
 
-TOLERANCE=71.0 ./build.sh || exit 1
-
-echo execute with tolerance 1000/71 +-0.42cm
-. ./execute_test.sh
-
 TOLERANCE=66.0 ./build.sh || exit 1
 
 echo execute with tolerance 1000/66 +-0.45cm
-. ./execute_test.sh
-
-TOLERANCE=62.0 ./build.sh || exit 1
-
-echo execute with tolerance 1000/66 +-0.48cm
 . ./execute_test.sh
 
 TOLERANCE=58.0 ./build.sh || exit 1
@@ -41,19 +37,9 @@ TOLERANCE=52.0 ./build.sh || exit 1
 echo execute with tolerance 1000/52 +-0.57cm
 . ./execute_test.sh
 
-TOLERANCE=50.0 ./build.sh || exit 1
-
-echo execute with tolerance 1000/50 +-0.6cm
-. ./execute_test.sh
-
 TOLERANCE=47.0 ./build.sh || exit 1
 
 echo execute with tolerance 1000/47 +-0.63cm
-. ./execute_test.sh
-
-TOLERANCE=45.0 ./build.sh || exit 1
-
-echo execute with tolerance 1000/45 +-0.67cm
 . ./execute_test.sh || exit 1
 
 exit 0
