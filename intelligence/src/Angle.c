27dea1b738f1204d0c03a72ab0ae1857a9c3f7eb
JBHarvey <jean.benoit.harvey@outlook.com>
2017-02-21 23:52:06 -0500

    Adds angle wrapping

--- intelligence/src/Angle.c
+++ intelligence/src/Angle.c
@@ -5,9 +5,27 @@
 struct Angle* Angle_new(int new_theta)
 {
     struct Angle * pointer = (struct Angle *) malloc(sizeof(struct Angle));
-    pointer->theta = new_theta;
+    int wrappedTheta = Angle_wrap(new_theta);
+    pointer->theta = wrappedTheta;
     return pointer;
 }
+
+/**
+ * Ensures the angle stays between MINUS_PI and PI.
+ */
+int Angle_wrap(int theta)
+{
+    while(theta > PI) {
+        theta -= 2 * PI;
+    }
+
+    while(theta < MINUS_PI) {
+        theta += 2 * PI;
+    }
+
+    return theta;
+}
+
 
 void Angle_delete(struct Angle* angle)
 {
--- intelligence/tests/AngleTest.c
+++ intelligence/tests/AngleTest.c
@@ -10,3 +10,21 @@ Test(Angle, creation_destruction)
     cr_assert(angle->theta == halfPi);
     Angle_delete(angle);
 }
+
+Test(Angle, given_aValueBiggerThanPi_when_created_then_valueIsWrappedBetweenPiAndMinusPi)
+{
+    const int fivePi = 1570795;
+    struct Angle* angle = Angle_new(fivePi);
+
+    cr_assert(angle->theta == PI);
+    Angle_delete(angle);
+}
+
+Test(Angle, given_aValueSmallerThanMinusPi_when_created_then_valueIsWrappedBetweenPiAndMinusPi)
+{
+    const int minusThreePi = -942477;
+    struct Angle* angle = Angle_new(minusThreePi);
+
+    cr_assert(angle->theta == MINUS_PI);
+    Angle_delete(angle);
+}
