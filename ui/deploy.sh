44713172930b415cabc8859ab655747c98d5c137
WhippetsAintDogs <marc.antoine.fortier.94@gmail.com>
2017-03-11 00:51:38 -0500

    autodeploy and network base integration

---
+++ ui/deploy.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+#Keep the previously built code
+#If nothing has changed, a file won't be recompiled
+rm -rf autodeploy/obj
+mkdir autodeploy/obj
+cp build/deploy/obj/* autodeploy/obj/.
+
+#Cleans the building directory
+rm -rf build/deploy
+mkdir -p build/deploy
+mkdir build/deploy/obj
+mkdir build/deploy/station-resources
+
+#Updates shared code
+cp ../shared/CommunicationStructures.h src
+
+#Prepares compilation files
+find src -type f -regex '.*\.\(c\|\h\)' -exec cp \{\} build/deploy \;
+cp station-main.c build/deploy/.
+
+#Prepares the list of file to check for dependency
+echo "DEPS=" > autodeploy/DEPS
+ls -lAh --color=never --format=single-column build/deploy | grep "\.h" --color=never >> autodeploy/DEPS
+
+#Prepares the list of file to build
+echo "_OBJ=" > autodeploy/_OBJ
+echo "station-main.h" >> autodeploy/_OBJ
+ls -lAh --color=never --format=single-column build/deploy | grep "\.h" --color=never >> autodeploy/_OBJ
+
+#Remplaces all \n with ' '
+sed -i ':a;N;$!ba;s/\n/ /g' autodeploy/DEPS
+
+#Does the same but also replaces all the .h with .o
+sed -i ':a;N;$!ba;s/\.h/.o/g' autodeploy/_OBJ
+sed -i ':a;N;$!ba;s/\n/ /g' autodeploy/_OBJ
+
+rm autodeploy/makefile
+
+#Takes the formated file names and puts them in the Makefile
+sed -e '/HFILES/{r autodeploy/DEPS' -e 'd}' autodeploy/templatefile >> autodeploy/tmpmakefile
+sed -e '/OFILES/{r autodeploy/_OBJ' -e 'd}' autodeploy/tmpmakefile >> autodeploy/makefile
+
+rm autodeploy/tmpmakefile
+
+#Copies the finalized files for compilation in the build directory
+cp autodeploy/makefile build/deploy/makefile
+cp autodeploy/obj/* build/deploy/obj/.
+cp src/station-resources/* build/deploy/station-resources/.
+
+#Launches the build
+cd build/deploy
+make all 
+
+#Brings back the runnable and runs it
+cd ../..
+cp build/deploy/station-main .
+./station-main
---
+++ ui/src/CommunicationStructures.h
@@ -0,0 +1,53 @@
+#ifndef COMMUNICATIONSTRUCTURES_H_
+#define COMMUNICATIONSTRUCTURES_H_
+
+enum {
+    PACKET_START,
+    PACKET_CONTINUE,
+    PACKET_WORLD
+};
+
+void sendStartPacket();
+
+struct __attribute__((__packed__)) Communication_Coordinates {
+    int x;
+    int y;
+};
+
+struct __attribute__((__packed__)) Communication_Pose {
+    struct Communication_Coordinates coordinates;
+    int theta;
+};
+
+struct __attribute__((__packed__)) Communication_Zone {
+    struct Communication_Pose pose;
+    int index;
+};
+
+struct __attribute__((__packed__)) Communication_Object {
+    struct Communication_Zone zone;
+    int radius;
+};
+
+struct __attribute__((__packed__)) Communication_Environment {
+    struct Communication_Coordinates north_eastern_table_corner;
+    struct Communication_Coordinates north_western_table_corner;
+    struct Communication_Coordinates south_eastern_table_corner;
+    struct Communication_Coordinates south_western_table_corner;
+    struct Communication_Coordinates north_eastern_drawing_corner;
+    struct Communication_Coordinates north_western_drawing_corner;
+    struct Communication_Coordinates south_eastern_drawing_corner;
+    struct Communication_Coordinates south_western_drawing_corner;
+    struct Communication_Coordinates antenna_zone_start;
+    struct Communication_Coordinates antenna_zone_stop;
+    struct Communication_Object obstacles[3];
+    struct Communication_Zone painting_zone[8];
+};
+
+struct __attribute__((__packed__)) Communication_World {
+    struct Communication_Environment environment;
+    int environment_has_changed;
+    struct Communication_Object robot;
+};
+
+#endif // COMMUNICATIONSTRUCTURES_H_
