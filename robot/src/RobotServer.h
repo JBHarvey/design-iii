99b79ff397069bc605ee26d2e216cd5f1afc2b35
JBHarvey <jean.benoit.harvey@outlook.com>
2017-03-10 23:26:48 -0500

    Adds Actuator

--- robot/src/RobotServer.h
+++ robot/src/RobotServer.h
@@ -11,24 +11,13 @@ struct RobotServer {
     struct Robot *robot;
     int port;
 };
-/*
-struct RobotServer *RobotServer_initClient(char *ip, unsigned short port);
-struct RobotServer *RobotServer_initServer(unsigned short port);
-*/
 struct RobotServer *RobotServer_new(struct Robot *new_robot, int new_port);
 void RobotServer_delete(struct RobotServer *robot_server);
 
 /*
 void RobotServer_do(struct RobotServer *robot_server, unsigned int milliseconds);
-void RobotServer_close(struct RobotServer *robot_server);
-*/
-/*
-FROM Protocol:
 
-void sendStartPacket();
-void sendContinuePacket();
 void sendWorldToRobot(struct Communication_World communication_world);
-
 
    */
 #endif // ROBOTSERVER_H_
