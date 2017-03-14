b4bd4bfad2e843eaf7226fd0fcd2ba8331584fc7
yan-m <yannik.marek.1@ulaval.ca>
2017-03-14 16:32:00 -0400

    More solid packet handling.

--- robot/robot-main.c
+++ robot/robot-main.c
@@ -3,16 +3,22 @@
 
 struct RobotServer *robot_server;
 const int port = 35794;
+const char *ttyACM = "test";//"/dev/ttyACM0";
 
 int main(int argc, char *argv[])
 {
     struct Robot *robot = Robot_new();
-    robot_server = RobotServer_new(robot, port);
+    robot_server = RobotServer_new(robot, port, ttyACM);
 
-    //    while(1) {
-    //      RobotServer_communicate(robot_server, 75);
-    printf("IT LIVES\n");
-    //   }
+    uint64_t data = 0;
+    printf("writing packet\n");
+    if(writeTTYACMPacket(robot_server, 1, &data, sizeof(data))) {
+        printf("wrote packet\n");
+    }
+
+    while(1) {
+        RobotServer_communicate(robot_server);
+    }
 
     RobotServer_delete(robot_server);
     Robot_delete(robot);
--- robot/src/RobotServer.h
+++ robot/src/RobotServer.h
@@ -8,10 +8,15 @@ struct RobotServer {
     struct Object *object;
     struct ev_loop *loop;
     struct Robot *robot;
+    int fd_ttyACM;
     int port;
 };
-struct RobotServer *RobotServer_new(struct Robot *new_robot, int new_port);
+struct RobotServer *RobotServer_new(struct Robot *new_robot, int new_port, char *ttyacm_path);
 void RobotServer_delete(struct RobotServer *robot_server);
+
+void RobotServer_communicate(struct RobotServer *robot_server);
+
+_Bool writeTTYACMPacket(struct RobotServer *robot_server, uint8_t type, uint8_t *data, unsigned int length);
 
 /*
 void RobotServer_do(struct RobotServer *robot_server, unsigned int milliseconds);
