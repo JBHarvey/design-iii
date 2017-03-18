47c1e2cc6bb56a92b96445417cae842438f2751a
WhippetsAintDogs <marc.antoine.fortier.94@gmail.com>
2017-03-17 21:13:46 -0400

    Chrono done

---
+++ ui/src/timer.h
@@ -0,0 +1,15 @@
+#ifndef __TIMER
+#define __TIMER
+
+#include <gtk/gtk.h>
+
+void Timer_initialize(GtkLabel *label);
+
+void Timer_start(void);
+
+void Timer_release(void);
+
+void Timer_redraw(void);
+
+#endif // __TIMER
+
