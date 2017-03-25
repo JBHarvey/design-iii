b43102b18ba4b4345f45ebf0ad11ba07266eae10
yan-m <yannik.marek.1@ulaval.ca>
2017-03-25 16:08:47 -0400

    Convert coordinates.

--- robot/tests/OnboardCamera.c
+++ robot/tests/OnboardCamera.c
@@ -1,16 +1,16 @@
 
 #include <criterion/criterion.h>
 #include <stdio.h>
-#include "Camera.h"
+#include "OnboardCamera.h"
 struct RobotServer *robot_server;
 
-Test(Camera, cvSeqToCoordinatesSequence)
+Test(OnboardCamera, cvSeqToCoordinatesSequence)
 {
     CvMemStorage *opencv_storage = cvCreateMemStorage(0);
-    CvSeq *opencv_sequence = (CvSeq *)cvLoad("../../utils/opencv_sequence.xml", opencv_storage, 0, 0);
-    struct Coordinates *coordinates = Coordinates_new(282, 233);
+    CvSeq *opencv_sequence = (CvSeq *)cvLoad("../utils/opencv_sequence.xml", opencv_storage, 0, 0);
+    struct Coordinates *coordinates = Coordinates_new(-322, -394);
 
-    struct CoordinatesSequence *coordinate_sequence = Camera_cvSeqToCoordinatesSequence(opencv_sequence);
+    struct CoordinatesSequence *coordinate_sequence = OnboardCamera_cvSeqToCoordinatesSequence(opencv_sequence);
 
     cr_assert(Coordinates_haveTheSameValues(coordinate_sequence->coordinates, coordinates));
     cr_assert_eq(CoordinatesSequence_size(coordinate_sequence), 9);
