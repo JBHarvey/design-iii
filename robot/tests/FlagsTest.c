afa6c458cbba60a3f209b55a6ff5a21b546bd28a
JBHarvey <jean.benoit.harvey@outlook.com>
2017-03-27 01:31:42 -0400

    Adds new flags and completes tests

--- robot/tests/FlagsTest.c
+++ robot/tests/FlagsTest.c
@@ -9,6 +9,9 @@ const int START_CYCLE = 1;
 const int PICTURE_TAKEN = 1;
 const int IMAGE_RECEIVED_BY_STATION = 1;
 const int PLANNED_TRAJECTORY_RECEIVED_BY_STATION = 1;
+const int READY_TO_START_RECEIVED_BY_STATION = 1;
+const int READY_TO_DRAW_RECEIVED_BY_STATION = 1;
+const int END_OF_CYCLE_RECEIVED_BY_STATION = 1;
 const int MINUS_ONE = -1;
 
 Test(Flags, creation_destruction)
@@ -25,13 +28,22 @@ void setup_flags(void)
 {
     flags = Flags_new();
     other_flags = Flags_new();
+
     Flags_setStartCycleSignalReceived(flags, START_CYCLE);
     Flags_setImageReceivedByStation(flags, IMAGE_RECEIVED_BY_STATION);
+    Flags_setPictureTaken(flags, PLANNED_TRAJECTORY_RECEIVED_BY_STATION);
     Flags_setPlannedTrajectoryReceivedByStation(flags, PLANNED_TRAJECTORY_RECEIVED_BY_STATION);
+    Flags_setReadyToStartReceivedByStation(flags, READY_TO_START_RECEIVED_BY_STATION);
+    Flags_setReadyToDrawReceivedByStation(flags, READY_TO_DRAW_RECEIVED_BY_STATION);
+    Flags_setEndOfCycleReceivedByStation(flags, END_OF_CYCLE_RECEIVED_BY_STATION);
+
     Flags_setStartCycleSignalReceived(other_flags, START_CYCLE);
     Flags_setImageReceivedByStation(other_flags, IMAGE_RECEIVED_BY_STATION);
+    Flags_setPictureTaken(other_flags, PLANNED_TRAJECTORY_RECEIVED_BY_STATION);
     Flags_setPlannedTrajectoryReceivedByStation(other_flags, PLANNED_TRAJECTORY_RECEIVED_BY_STATION);
-
+    Flags_setReadyToStartReceivedByStation(other_flags, READY_TO_START_RECEIVED_BY_STATION);
+    Flags_setReadyToDrawReceivedByStation(other_flags, READY_TO_DRAW_RECEIVED_BY_STATION);
+    Flags_setEndOfCycleReceivedByStation(other_flags, END_OF_CYCLE_RECEIVED_BY_STATION);
 }
 
 void teardown_flags(void)
@@ -53,6 +65,12 @@ Test(Flags, given_twoFlagsWithDifferentValues_when_comparesThem_then_returnsFals
      , .fini = teardown_flags)
 {
     Flags_setStartCycleSignalReceived(flags, ZERO);
+    Flags_setPictureTaken(flags, ZERO);
+    Flags_setImageReceivedByStation(flags, ZERO);
+    Flags_setPlannedTrajectoryReceivedByStation(flags, ZERO);
+    Flags_setReadyToStartReceivedByStation(flags, ZERO);
+    Flags_setReadyToDrawReceivedByStation(flags, ZERO);
+    Flags_setEndOfCycleReceivedByStation(flags, ZERO);
 
     int comparison = Flags_haveTheSameValues(flags, other_flags);
     cr_assert(comparison == 0);
@@ -63,6 +81,12 @@ Test(Flags, given_twoFlagsWithDifferentValues_when_oneHasMinusOnesInItsValuesAnd
      , .fini = teardown_flags)
 {
     Flags_setStartCycleSignalReceived(flags, MINUS_ONE);
+    Flags_setPictureTaken(flags, MINUS_ONE);
+    Flags_setImageReceivedByStation(flags, MINUS_ONE);
+    Flags_setPlannedTrajectoryReceivedByStation(flags, MINUS_ONE);
+    Flags_setReadyToStartReceivedByStation(flags, MINUS_ONE);
+    Flags_setReadyToDrawReceivedByStation(flags, MINUS_ONE);
+    Flags_setEndOfCycleReceivedByStation(flags, MINUS_ONE);
 
     int comparison = Flags_haveTheSameValues(flags, other_flags);
     cr_assert(comparison == 1);
@@ -73,6 +97,12 @@ Test(Flags, given_twoFlagsWithDifferentValues_when_copied_then_theFlagsHaveTheSa
      , .fini = teardown_flags)
 {
     Flags_setStartCycleSignalReceived(flags, ZERO);
+    Flags_setPictureTaken(flags, ZERO);
+    Flags_setImageReceivedByStation(flags, ZERO);
+    Flags_setPlannedTrajectoryReceivedByStation(flags, ZERO);
+    Flags_setReadyToStartReceivedByStation(flags, ZERO);
+    Flags_setReadyToDrawReceivedByStation(flags, ZERO);
+    Flags_setEndOfCycleReceivedByStation(flags, ZERO);
 
     Flags_copyValuesFrom(flags, other_flags);
 
@@ -109,7 +139,33 @@ Test(Flags_flag,
      , .init = setup_flags
      , .fini = teardown_flags)
 {
-    Flags_setImageReceivedByStation(flags, PLANNED_TRAJECTORY_RECEIVED_BY_STATION);
-    cr_assert(flags->image_received_by_station == PLANNED_TRAJECTORY_RECEIVED_BY_STATION);
+    Flags_setPlannedTrajectoryReceivedByStation(flags, PLANNED_TRAJECTORY_RECEIVED_BY_STATION);
+    cr_assert(flags->planned_trajectory_received_by_station == PLANNED_TRAJECTORY_RECEIVED_BY_STATION);
 }
 
+Test(Flags_flag,
+     given_defaultFlags_when_triesToSetReadyToStartReceivedByStationToOne_then_readyToStartReceivedByStationIsOne
+     , .init = setup_flags
+     , .fini = teardown_flags)
+{
+    Flags_setReadyToStartReceivedByStation(flags, READY_TO_START_RECEIVED_BY_STATION);
+    cr_assert(flags->ready_to_start_received_by_station == READY_TO_START_RECEIVED_BY_STATION);
+}
+
+Test(Flags_flag,
+     given_defaultFlags_when_triesToSetReadyToDrawReceivedByStationToOne_then_readyToStartReceivedByStationIsOne
+     , .init = setup_flags
+     , .fini = teardown_flags)
+{
+    Flags_setReadyToDrawReceivedByStation(flags, READY_TO_DRAW_RECEIVED_BY_STATION);
+    cr_assert(flags->ready_to_draw_received_by_station == READY_TO_DRAW_RECEIVED_BY_STATION);
+}
+
+Test(Flags_flag,
+     given_defaultFlags_when_triesToSetEndOfCycleReceivedByStationToOne_then_endOfCycleReceivedByStationIsOne
+     , .init = setup_flags
+     , .fini = teardown_flags)
+{
+    Flags_setEndOfCycleReceivedByStation(flags, END_OF_CYCLE_RECEIVED_BY_STATION);
+    cr_assert(flags->end_of_cycle_received_by_station == END_OF_CYCLE_RECEIVED_BY_STATION);
+}
