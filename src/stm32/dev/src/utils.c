9c924f9cdfc60d63f5b1ccb8bb29314de551fa42
Martin31416 <Jesus_SoldierZ@hotmail.com>
2017-03-26 21:45:04 -0400

    utils .c

--- src/stm32/dev/src/utils.c
+++ src/stm32/dev/src/utils.c
@@ -18,46 +18,44 @@ void cleanNumberString(char *numberString, int size) {
  * IN : the new state
  */
 void setState(int* state, int newState) {
-	switch (newState) {
-	case MAIN_IDLE:
-		*state = newState;
-		TM_HD44780_Puts(0, 0, MSG_IDLE);
-		break;
-	case MAIN_MANCH:
-		*state = newState;
-		TM_HD44780_Puts(0, 0, MSG_MANCH);
-		break;
-	case MAIN_ACQUIS:
-		TM_HD44780_Puts(0, 0, "NOT IMPLEMENTED");
-		break;
-	case MAIN_ACQUIS_ALL:
-		*state = MAIN_ACQUIS_ALL;
-		break;
-	case MAIN_TEST_SPEED_PID:
-		*state = MAIN_TEST_SPEED_PID;
-		break;
-	case MAIN_TEST_POS_PID:
-		*state = MAIN_TEST_POS_PID;
-		break;
-	case MAIN_TEST_DEAD_ZONE:
-		*state = MAIN_TEST_DEAD_ZONE;
-		break;
-	case MAIN_TEST_ROTATION:
-		*state = MAIN_TEST_ROTATION;
-		break;
-	case MAIN_MOVE:
-		*state = MAIN_MOVE;
-		TM_HD44780_Puts(0, 0, "MOVE");
-		break;
-	case MAIN_PID:
-		*state = newState;
-		TM_HD44780_Puts(0, 0, "NOT IMPLEMENTED");
-		break;
-	default:
-		// Invalid state
-		TM_HD44780_Puts(0, 0, MSG_ERR);
-		break;
-	}
+	*state = newState;
+
+	/*switch (newState) {
+	 case MAIN_IDLE:
+	 *state = newState;
+	 TM_HD44780_Puts(0, 0, MSG_IDLE);
+	 break;
+	 case MAIN_MANCH:
+	 *state = newState;
+	 TM_HD44780_Puts(0, 0, MSG_MANCH);
+	 break;
+	 case MAIN_ACQUIS:
+	 TM_HD44780_Puts(0, 0, "NOT IMPLEMENTED");
+	 break;
+	 case MAIN_ACQUIS_ALL:
+	 *state = MAIN_ACQUIS_ALL;
+	 break;
+	 case MAIN_TEST_SPEED_PID:
+	 *state = MAIN_TEST_SPEED_PID;
+	 break;
+	 case MAIN_TEST_POS_PID:
+	 *state = MAIN_TEST_POS_PID;
+	 break;
+	 case MAIN_TEST_DEAD_ZONE:
+	 *state = MAIN_TEST_DEAD_ZONE;
+	 break;
+	 case MAIN_MOVE:
+	 *state = MAIN_MOVE;
+	 TM_HD44780_Puts(0, 0, "MOVE");
+	 break;
+	 case MAIN_PID:
+	 *state = newState;
+	 TM_HD44780_Puts(0, 0, "NOT IMPLEMENTED");
+	 break;
+	 default:
+	 // Invalid state
+	 TM_HD44780_Puts(0, 0, MSG_ERR);
+	 break;*/
 }
 
 float sqroot(float square) {
