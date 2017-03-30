5b9ae67f34d6d3dd2d66bf7b6d8f40acc47dafd8
JBHarvey <jean.benoit.harvey@outlook.com>
2017-03-30 06:54:54 -0400

    Flash Robot

--- robot/src/Navigator.c
+++ robot/src/Navigator.c
@@ -44,10 +44,7 @@ void Navigator_updateNavigableMap(struct Robot *robot)
 {
     int map_has_been_updated = robot->world_camera->map_sensor->has_received_new_data;
 
-    Flags_setNavigableMapIsReady(robot->current_state->flags, map_has_been_updated);
-
     if(map_has_been_updated) {
-        Robot_sendReadyToStartSignal(robot);
         struct Map *base_map = robot->world_camera->map;
         int robot_radius = robot->world_camera->robot_radius;
         robot->navigator->navigable_map = Map_fetchNavigableMap(base_map, robot_radius);
@@ -56,6 +53,8 @@ void Navigator_updateNavigableMap(struct Robot *robot)
         // TODO: add validation that map is navigable && robot can go through the obstacles
         // Add to tests
     }
+
+    Flags_setNavigableMapIsReady(robot->current_state->flags, map_has_been_updated);
 }
 
 int Navigator_isAngleWithinRotationTolerance(int angle)
