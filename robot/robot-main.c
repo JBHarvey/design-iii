60e4c8ecaef4baf6886fa044f9174ea72f1ebf79
JBHarvey <jean.benoit.harvey@outlook.com>
2017-04-10 09:47:02 -0400

    Flash Robot

--- robot/robot-main.c
+++ robot/robot-main.c
@@ -8,7 +8,63 @@ struct PoseFilter *pose_filter;
 struct Robot *robot;
 struct RobotServer *robot_server;
 struct Timer *timer;
-struct PoseFilter_Callbacks filters;
+
+int main(int argc, char *argv[])
+{
+    const int port = 35794;
+    //char *ttyACM = "/dev/null";
+    char *ttyACM = "/dev/ttyACM0";
+
+    struct PoseFilter_Callbacks filters = PoseFilter_fetchCallbacks();
+
+    OnboardCamera_init();
+    robot = Robot_new();
+    pose_filter = PoseFilter_new(robot);
+    robot_server = RobotServer_new(robot, port, ttyACM);
+
+    timer = Timer_new();
+
+    Logger_startLoggingRobot(robot);
+
+    while(1) {
+
+        RobotServer_communicate(robot_server);
+
+        if(pose_filter->robot->current_state->flags->navigable_map_is_ready) {
+            PoseFilter_executeFilter(pose_filter, filters.updateFromCameraOnly);
+            Robot_resetAllActuators(robot);
+        }
+
+        Robot_updateBehaviorIfNeeded(robot);
+        Robot_act(robot);
+
+        if(Timer_hasTimePassed(timer, ONE_TENTH_OF_A_SECOND)) {
+            Robot_sendPoseEstimate(robot);
+            Timer_reset(timer);
+        }
+
+        //ask station to send : [stop signal] on exit
+
+        /*
+
+        initializeRobot();
+
+        executeRobot();
+
+        freeRobot();
+        */
+    }
+
+    Timer_delete(timer);
+    RobotServer_delete(robot_server);
+    Robot_delete(robot);
+    PoseFilter_delete(pose_filter);
+    OnboardCamera_freeCamera();
+
+
+    return 0;
+}
+/*
 
 void initializeRobot(void)
 {
@@ -58,57 +114,4 @@ void freeRobot(void)
     PoseFilter_delete(pose_filter);
     OnboardCamera_freeCamera();
 }
-
-int main(int argc, char *argv[])
-{
-    const int port = 35794;
-    //char *ttyACM = "/dev/null";
-    char *ttyACM = "/dev/ttyACM0";
-
-    filters = PoseFilter_fetchCallbacks();
-
-    OnboardCamera_init();
-    robot = Robot_new();
-    pose_filter = PoseFilter_new(robot);
-    robot_server = RobotServer_new(robot, port, ttyACM);
-
-    timer = Timer_new();
-
-    Logger_startLoggingRobot(robot);
-
-    while(1) {
-
-        /*
-        initializeRobot();
-
-        executeRobot();
-
-        freeRobot();
-        */
-        RobotServer_communicate(robot_server);
-
-        if(pose_filter->robot->current_state->flags->navigable_map_is_ready) {
-            PoseFilter_executeFilter(pose_filter, filters.updateFromCameraOnly);
-            Robot_resetAllActuators(robot);
-        }
-
-        Robot_updateBehaviorIfNeeded(robot);
-        Robot_act(robot);
-
-        if(Timer_hasTimePassed(timer, ONE_TENTH_OF_A_SECOND)) {
-            Robot_sendPoseEstimate(robot);
-            Timer_reset(timer);
-        }
-
-        //ask station to send : [stop signal] on exit
-    }
-
-    Timer_delete(timer);
-    RobotServer_delete(robot_server);
-    Robot_delete(robot);
-    PoseFilter_delete(pose_filter);
-    OnboardCamera_freeCamera();
-
-
-    return 0;
-}
+*/
