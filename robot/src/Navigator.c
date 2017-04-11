c2dac4491f6d74707cf0a920013632d14ce3ca8b
WhippetsAintDogs <marc.antoine.fortier.94@gmail.com>
2017-04-10 21:30:57 -0400

    flash

--- robot/src/Navigator.c
+++ robot/src/Navigator.c
@@ -136,8 +136,11 @@ static int convertDistanceToSpeed(int distance, int current_speed)
     if(current_speed < top_speed) {
         //if(current_speed < SHORT_DISTANCE) {
         //    speed = 150;
-
-        speed = (current_speed * ACCELERATION_FACTOR < top_speed) ? current_speed * ACCELERATION_FACTOR : top_speed;
+        if(current_speed < 0) {
+            speed = (current_speed * ACCELERATION_FACTOR > -top_speed) ? current_speed * ACCELERATION_FACTOR : -top_speed;
+        } else {
+            speed = (current_speed * ACCELERATION_FACTOR < top_speed) ? current_speed * ACCELERATION_FACTOR : top_speed;
+        }
     }
 
     return speed;
